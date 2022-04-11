03a7b1ea9ead89b170c3122f7283af7d203ec116
Deokhaeng Lee <boyohaeng@gmail.com>
2022-04-11 15:31:53 +0900

    로그인 오류

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,11 @@
+// src/setupProxy.js
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://localhost:5000",
+    // "/user",
+    createProxyMiddleware("/", {
+      target: "http://localhost:6000",
       changeOrigin: true,
     })
   );
