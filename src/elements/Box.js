e70c7887fc11832f46d44d6bf1d6e038d8ea9beb
yooDeve <96562253+yoodeve@users.noreply.github.com>
2022-04-14 18:26:25 +0900

    박스 업데이트

--- src/elements/Box.js
+++ src/elements/Box.js
@@ -1,48 +1,47 @@
 import React from "react";
 import styled from "styled-components";
+import '../shared/App.css';
 
 const Box = (props) => {
-    const {padding, margin, float, _onClick, children} = props;
-    const styles = {
-        padding:padding,
-        margin:margin,
-        float,
-        _onClick,
-    }
-    return (
-        <React.Fragment>
-            <BoxDiv {...styles} onClick={_onClick}>
-                {children}
-            </BoxDiv>
-        </React.Fragment>
-    )
-}
+  const { padding, margin, float, _onClick, children } = props;
+  const styles = {
+    padding: padding,
+    margin: margin,
+    float,
+    _onClick,
+  };
+  return (
+    <React.Fragment>
+      <BoxDiv {...styles} onClick={_onClick}>
+        {children}
+      </BoxDiv>
+    </React.Fragment>
+  );
+};
 
 Box.defaultProps = {
-    children:null,
-    padding:'16px',
-    margin:'20px auto',
-    float:false,
-    _onClick:()=>{},
-}
+  children: null,
+  padding: "16px",
+  margin: "20px auto",
+  float: false,
+  _onClick: () => {},
+};
 
-//카드박스전체를 감싸는 박스 
+//카드박스전체를 감싸는 박스
 const BoxDiv = styled.div`
-max-width: 350px;
-min-height: 60vh;
-background-color: #2B2B2B;
-padding: ${(props)=>props.padding};
-margin: ${(props)=>props.margin};
-border-radius: 5px;
-border: 1px solid #FFE05D;
-box-shadow: #FFE05D  2.5px 2.5px 2.5px;
-flex-wrap:wrap;
-flex-direction: row;
-
-
-${(props) => (props.float ? `float:left;` : "")}
-`
+  max-width: 1000px;
+  width:400px;
+  height: 60vh;
+  background-color: white;
+  padding: ${(props) => props.padding};
+  margin: ${(props) => props.margin};
+  border-radius: 5px;
+  border: none;
+  box-shadow: 0px 0px 10px 0px gray;
+  flex-wrap: wrap;
+  flex-direction: row;
+  ${(props) => (props.float ? `float:left;` : "")}
+  font-family: 'Cafe24Ohsquareair';
+`;
 
 export default Box;
-
-
