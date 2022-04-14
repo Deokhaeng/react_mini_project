3dfdc07749e336618099a5b6c325ed4b089eacc0
yooDeve <96562253+yoodeve@users.noreply.github.com>
2022-04-14 18:31:33 +0900

    본인게시물 삭제기능 구현 완료

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,86 +1,45 @@
-// import React from "react";
-// import { useDispatch } from "react-redux";
-// import { Grid, Image, Text, Box, Button } from "../elements/index";
-// import instance from "../shared/Request";
-// import { history } from "../redux/configureStore";
-
-// import { actionCreators as deleteActions } from "../redux/modules/post";
-
-
-// const Post = (props) => {
-// const dispatch = useDispatch()
-// const Delete = (e) =>{
-//   e.stopPropagation();
-//   dispatch(deleteActions.getPostDetailDB(props.id)) 
-// };
-
-// const postApi = {
-//   getPost:() => instance.get('/api/getlist')
-// }
-
-//   return (
-//     <React.Fragment>
-//       {/* Box:전체를 감싸는 카드박스 */}
-//       <Grid margin='60px auto' _onClick={(post_id)=>{history.push(`/detail/${postApi.post_id}`)}}>
-//         <Box float margin='60px'>
-//         <Grid padding="3px">
-//           <Text>{postApi.createdAt}</Text>
-//         </Grid>
-//         <Grid>
-//           <Image src={postApi.image} is_contents/>
-//         </Grid>
-//         <Grid>
-//           <Text bold="700">{postApi.title}</Text>
-//           <Text>{postApi.content}</Text>
-//         </Grid>
-//       </Box>
-//       </Grid>
-      
-//     </React.Fragment>
-//   );
-// };
-
-// Post.defaultProps = {
-//   user_name: "yoo",
-//   image_url:
-//     "https://recipe1.ezmember.co.kr/cache/recipe/2016/10/07/f43063e718c49d85ddce4880e4a41fcd1.jpg",
-//   title: "떡볶이 이름",
-//   createdAt: "2022-04-08",
-//   content:
-//     "같이 인생에 보배를 아니더면, 할지라도 만천하의 낙원을 얼마나 원대하고, 힘있다. 심장은 희망의 얼음과 천하를 때문이다. 무엇이 청춘을 같이, 가슴이 그들을 발휘하기 설레는 살았으며, 생명을 이것이다. 영원히 커다란 쓸쓸한 사막이다. 실로 없으면, 풀이 꽃이 이상의 품으며, 기쁘며, 하는 생생하며, 이것이다. 이것이야말로 무엇을 꽃이 뛰노는 인간의 있는 싹이 무엇을 피부가 것이다. 위하여서, 옷을 살 있으랴?",
-// };
-
-// export default Post;
 
 import React from "react";
 import { Grid, Image, Text, Button, Box } from "../elements";
 import { history } from "../redux/configureStore";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { actionCreators as deleteActions } from "../redux/modules/post";
-
+import user, { actionCreators as userActions } from "../redux/modules/user";
+import OutDiv from "../elements/OutDiv";
 const Post = (props) => {
   const dispatch = useDispatch();
-
+  const post_list = useSelector((state) => state.post.list);
+  // console.log(post_list)
+  console.log(props.id);
+  const user_id = useSelector((state) => state.user.user_id);
+  console.log(user_id);
+  React.useEffect(() => {
+    dispatch(userActions.getUserDB());
+  }, []);
   const onClick = (e) => {
     e.stopPropagation();
-    history.push(`/modify/${props.id}`);
+    history.push(`/modify/${props.post_id}`);
   };
-
   const Delete = (e) => {
     e.stopPropagation();
-    dispatch(deleteActions.deletePostFB(props.id));
+    dispatch(deleteActions.deletePostDB(props.post_id));
+    console.log(props.post_id)
   };
-
   return (
     <React.Fragment>
-      {/* <Grid padding="20px">
+      <Box>
+        <Grid
+        _onClick={() => {
+          history.push(`/detail/${props.post_id}`);
+        }}
+      >
         <Grid is_flex width="auto">
           <Image shape="circle" src={props.src} />
-          <Text bold>{props.user_info.user_name}</Text>
-          <Text>{props.insert_dt}</Text>
+          <Text bold>{props.id}</Text>
+          <Text>{props.createdAt}</Text>
         </Grid>
-        <Grid is_flex width="auto" margin="0% 0% 0% 48%">
-          {props.is_me && (
+        {user_id===props.id && (
+          <Grid is_flex width="auto" margin="0% 0% 0% 48%">
             <Button
               width="40px"
               padding="5px 0px"
@@ -88,82 +47,34 @@ const Post = (props) => {
               text="수정"
               _onClick={onClick}
             />
-          )}
-          {props.is_me && (
             <Button
               width="40px"
               padding="5px 0px"
               text="제거"
               _onClick={Delete}
             />
-          )}
-        </Grid>
-        <Grid padding="16px" width="auto">
-          <Text>{props.contents}</Text>
-        </Grid>
-        <Grid>
-          <Image shape="rectangle" src={props.image_url} />
-        </Grid>
-        <Grid padding="16px">
-          <Text bold>댓글 {props.comment_cnt}개</Text>
-        </Grid>
-      </Grid> */}
-      <Box margin='150px 3px 3px 30px' _onClick={()=>{history.push(`/detail/${props.post_id}`)}}>
-        <Grid padding="3px" is_flex>
-          <Text bold="700">{props.id}</Text>
-          <Grid is_flex width="auto" margin="0% 0% 0% 48%">
-            {props.is_me && (
-              <Button
-                width="40px"
-                padding="5px 0px"
-                margin="0% 3px 0% 70%"
-                text="수정"
-                _onClick={onClick}
-              />
-            )}
-            {props.is_me && (
-              <Button
-                width="40px"
-                padding="5px 0px"
-                text="제거"
-                _onClick={Delete}
-              />
-            )}
           </Grid>
-          <Text>{props.createdAt}</Text>
-        </Grid>
-        <Grid>
-          <Image src={props.src} />
-        </Grid>
-        <Grid>
-          <Text bold="700">{props.title}</Text>
+        )}
+        <Grid padding="16px" width="auto">
+          <Text bold>{props.title}</Text>
           <Text>{props.content}</Text>
         </Grid>
+        <Grid>
+          <Image shape="rectangle" src={props.image} />
+        </Grid>
+      </Grid>
       </Box>
     </React.Fragment>
   );
 };
-
-// Post.defaultProps = {
-//   user_info: {
-//     user_name: "mandu",
-//     user_profile: "https://ifh.cc/g/AOA4Wq.jpg",
-//   },
-//   img_url: "https://ifh.cc/g/AOA4Wq.jpg",
-//   contents: "왕귀여운 토끼",
-//   insert_dt: "2022-04-01",
-//   comment_cnt: "10",
-//   is_me: false,
-// };
-
 Post.defaultProps = {
-  id: "yoo",
-  image_url:
-    "https://recipe1.ezmember.co.kr/cache/recipe/2016/10/07/f43063e718c49d85ddce4880e4a41fcd1.jpg",
-  title: "떡볶이 이름",
-  createdAt: "2022-04-08",
-  content:
-    "같이 인생에 보배를 아니더면, 할지라도 만천하의 낙원을 얼마나 원대하고, 힘있다. 심장은 희망의 얼음과 천하를 때문이다. 무엇이 청춘을 같이, 가슴이 그들을 발휘하기 설레는 살았으며, 생명을 이것이다. 영원히 커다란 쓸쓸한 사막이다. 실로 없으면, 풀이 꽃이 이상의 품으며, 기쁘며, 하는 생생하며, 이것이다. 이것이야말로 무엇을 꽃이 뛰노는 인간의 있는 싹이 무엇을 피부가 것이다. 위하여서, 옷을 살 있으랴?",
+  id: "1",
+  post_id: "mandu31",
+  image: "https://ifh.cc/g/AOA4Wq.jpg",
+  title: "토끼만만세",
+  content: "왕귀여운 토끼",
+  createdAt: "2022-04-01",
+  comment_cnt: "10",
+  is_login: false,
 };
-
 export default Post;
