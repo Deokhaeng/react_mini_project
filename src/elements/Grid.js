8918e8a626f9b485f020dd48b3684b1a434fdacd
yooDeve <96562253+yoodeve@users.noreply.github.com>
2022-04-14 18:27:58 +0900

    그리드 업데이트

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -30,7 +30,9 @@ const Grid = (props) => {
   };
   return (
     <React.Fragment>
-      <GridBox {...styles} onClick={_onClick}>{children}</GridBox>
+      <GridBox {...styles} onClick={_onClick}>
+        {children}
+      </GridBox>
     </React.Fragment>
   );
 };
@@ -40,6 +42,7 @@ Grid.defaultProps = {
   is_flex: false,
   fixed: false,
   margin_left: false,
+  _onClick: () => {},
   width: "100%",
   padding: false,
   margin: false,
@@ -52,16 +55,13 @@ const GridBox = styled.div`
   width: ${(props) => props.width};
   height: ${(props) => props.height};
   box-sizing: border-box;
-
   ${(props) => (props.padding ? `padding:${props.padding}` : "")}
   ${(props) => (props.margin ? `margin:${props.margin}` : "")}
   ${(props) => (props.bg ? `background-color:${props.bg}` : "")}
-
   ${(props) =>
     props.is_flex
       ? `display: flex; align-items: center; justify-content:space-between;`
       : ""}
-
   ${(props) =>
     props.fixed
       ? `
