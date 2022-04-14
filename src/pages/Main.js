648f150e81ac73204c96445e3b1227a78b96d909
yooDeve <96562253+yoodeve@users.noreply.github.com>
2022-04-14 18:32:23 +0900

    OutDiv추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,46 +1,55 @@
 import React from "react";
-// import Post from "../components/Post";
+import { useSelector, useDispatch } from "react-redux";
 import Header from "../components/Header";
 import Post from "../components/Post";
-
+import { actionCreators as postActions } from "../redux/modules/post";
+import { actionCreators as userActions } from "../redux/modules/user";
 import { history } from "../redux/configureStore";
-import { useSelector, useDispatch } from "react-redux";
 
 import { Grid, Button } from "../elements";
+import OutDiv from "../elements/OutDiv";
 
-import { actionCreators as postActions } from "../redux/modules/post";
-
-const Main = () => {
+const Main = (props) => {
   const dispatch = useDispatch();
   const post_list = useSelector((state) => state.post.list);
-  console.log(post_list);
+
+  const is_login = useSelector((state) => state.user.is_login);
 
   React.useEffect(() => {
     if (post_list.length < 2) {
       //리스트에 길이가 있으면 getPost를 하지 않는다. => 이미 있던 리덕스 맨 앞에 추가가 됌.
-      dispatch(postActions.setPostDB());
+      dispatch(postActions.getPostDB());
     }
+
+    dispatch(userActions.getUserDB());
   }, []);
 
-  return (
-    <>
-      <Grid padding="1px">
-        {/* <Post /> */}
-        <Grid is_flex>
-        {post_list.map((post, idx) => {
-          return <Post key={post._id} {...post} />;
+  if (is_login) {
+    return (
+      <>
+        <Header />
+        <OutDiv>
+                  {post_list.map((p, idx) => {
+          return <Post key={p._id} {...p} />;
         })}
 
-        {/* {post_list} */}
-      </Grid>
-      </Grid>
-      <Button
-        is_float
-        text="+"
-        _onClick={() => {
-          history.push("/write");
-        }}
-      ></Button>
+        <Button
+          is_float
+          text="+"
+          _onClick={() => {
+            history.push("/write");
+          }}
+        ></Button>
+        </OutDiv>
+      </>
+    );
+  }
+  return (
+    <>
+      <Header />
+      {post_list.map((p, idx) => {
+        return <Post key={p._id} {...p} />;
+      })}
     </>
   );
 };
