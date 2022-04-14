1a16c4a82c966015b0335ca9681fd6e80d7b9831
yooDeve <96562253+yoodeve@users.noreply.github.com>
2022-04-14 18:27:02 +0900

    버튼 업데이트

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -2,21 +2,32 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = (props) => {
-  const { text, _onClick, is_float, is_delete, children, margin, width, padding, border, right } = props;
+  const {
+    text,
+    _onClick,
+    is_float,
+    is_delete,
+    children,
+    margin,
+    width,
+    padding,
+    border,
+    right,
+  } = props;
 
   if (is_float) {
     return (
       <React.Fragment>
-        <FloatButton onClick={_onClick}>{text? text : children}</FloatButton>
+        <FloatButton onClick={_onClick}>{text ? text : children}</FloatButton>
       </React.Fragment>
     );
   }
-  if(is_delete){
-    return(
+  if (is_delete) {
+    return (
       <React.Fragment>
-        <DeleteButton onClick={_onClick}>{text? text : children}</DeleteButton>
+        <DeleteButton onClick={_onClick}>{text ? text : children}</DeleteButton>
       </React.Fragment>
-    )
+    );
   }
 
   const styles = {
@@ -29,7 +40,9 @@ const Button = (props) => {
 
   return (
     <React.Fragment>
-      <ElButton {...styles} onClick={_onClick}>{text? text: children}</ElButton>
+      <ElButton {...styles} onClick={_onClick}>
+        {text ? text : children}
+      </ElButton>
     </React.Fragment>
   );
 };
@@ -40,27 +53,27 @@ Button.defaultProps = {
   _onClick: () => {},
   is_float: false,
   margin: false,
-  width: '100%',
-  padding: '12px 0px',
+  width: "100%",
+  padding: "12px 0px",
   border: false,
 };
 
 const ElButton = styled.button`
   width: ${(props) => props.width};
-  background-color: #FFE05D;
+  background-color: #ffe05d;
   color: #212121;
-  padding: ${(props)=>props.padding};
+  padding: ${(props) => props.padding};
   box-sizing: border-box;
-  border: #525E75 solid 1px;
-  ${(props) => (props.margin? `margin: ${props.margin};` : '')}
-  ${(props)=>props.right?`float: right`:''}
+  border: #525e75 solid 1px;
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+  ${(props) => (props.right ? `float: right` : "")}
 `;
 
 const FloatButton = styled.button`
   width: 50px;
   height: 50px;
-  background-color: #FFE05D;
-  color: #525E75;
+  background-color: #ffe05d;
+  color: #525e75;
   box-sizing: border-box;
   font-size: 36px;
   font-weight: 800;
@@ -69,15 +82,15 @@ const FloatButton = styled.button`
   right: 16px;
   text-align: center;
   vertical-align: middle;
-  border: #525E75;
+  border: #525e75;
   border-radius: 50px;
 `;
 
 const DeleteButton = styled.button`
   width: 40px;
   height: 20px;
-  background-color: #FFE05D;
-  color: #525E75;
+  background-color: #ffe05d;
+  color: #525e75;
   box-sizing: border-box;
   font-size: 12px;
   font-weight: 800;
@@ -86,8 +99,8 @@ const DeleteButton = styled.button`
   right: 16px;
   text-align: center;
   vertical-align: middle;
-  border: #525E75;
-  border-radius: 50x;  
+  border: #525e75;
+  border-radius: 50x;
 `;
 
 export default Button;
